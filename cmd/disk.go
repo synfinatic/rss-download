b05293215b576b68ff918409eac33bc511584c38
Aaron Turner <synfinatic@gmail.com>
2021-05-30 11:16:42 -0700

    Add `DiskBuffer` config option
    Need to support dealing with the 5% reserved for root or
    other cases where we want to leave some free space on the volume
    and not download files.

--- cmd/disk.go
+++ cmd/disk.go
@@ -5,16 +5,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/knadh/koanf"
 	log "github.com/sirupsen/logrus"
 	syscall "golang.org/x/sys/unix"
 )
 
 const (
-	B  = 1
-	KB = 1024 * B
-	MB = 1024 * KB
-	GB = 1024 * MB
+	B           = 1
+	KB          = 1024 * B
+	MB          = 1024 * KB
+	GB          = 1024 * MB
+	TB          = 1024 * GB
+	DISK_BUFFER = "DiskBuffer"
 )
 
 type DiskStatus struct {
@@ -25,26 +30,41 @@ type DiskStatus struct {
 }
 
 // disk usage of path/disk
-func DiskUsage(path string) (disk DiskStatus) {
-	fs := syscall.Statfs_t{}
-	err := syscall.Statfs(path, &fs)
+func DiskUsage(konf *koanf.Koanf, path string) (DiskStatus, error) {
+	diskBuffer, err := convertBytesString(konf.String(DISK_BUFFER))
 	if err != nil {
-		log.WithError(err).Errorf("Unable to Statfs: %s", path)
-		return
+		log.WithError(err).Errorf("Unable to apply %s", DISK_BUFFER)
+		diskBuffer = 0
+	}
+
+	disk := DiskStatus{}
+
+	fs := syscall.Statfs_t{}
+	err = syscall.Statfs(path, &fs)
+	if err != nil {
+		return DiskStatus{}, fmt.Errorf("Unable to statfs: %s: %s", path, err)
 	}
 	disk.All = fs.Blocks * uint64(fs.Bsize)
 	disk.Avail = fs.Bavail * uint64(fs.Bsize)
+
+	// apply our buffer
+	if disk.Avail < diskBuffer {
+		disk.Avail = 0
+	} else {
+		disk.Avail -= diskBuffer
+	}
+
 	disk.Free = fs.Bfree * uint64(fs.Bsize)
+
 	info, err := os.Lstat(path)
 	if err != nil {
-		log.WithError(err).Errorf("Unable to Lstat: %s", path)
-		return
+		return DiskStatus{}, fmt.Errorf("Unable to Lstat %s: %s", path, err.Error())
 	}
 	disk.Used, err = GetDirectorySize(path, info)
 	if err != nil {
-		log.WithError(err).Errorf("Unable to get directory size: '%s'", path)
+		return DiskStatus{}, fmt.Errorf("Unable to get directory size %s: %s", path, err.Error())
 	}
-	return
+	return disk, nil
 }
 
 func GetDirectorySize(path string, info os.FileInfo) (uint64, error) {
@@ -89,4 +109,35 @@ func (disk *DiskStatus) DiskInfo(newFileSize uint64) string {
 		color,
 		float64(disk.Avail)/float64(GB),
 		float64(disk.Used)/float64(GB))
+}
+
+// Converts XXGB/TB/MB/KB to number of bytes
+func convertBytesString(str string) (uint64, error) {
+	if str == "" {
+		return 0, nil
+	}
+	var zBytes uint64
+	var err error
+
+	if strings.HasSuffix(str, "TB") {
+		tb := strings.Trim(str, "TB")
+		zBytes, err = strconv.ParseUint(tb, 10, 64)
+		zBytes *= TB
+	} else if strings.HasSuffix(str, "GB") {
+		gb := strings.Trim(str, "GB")
+		zBytes, err = strconv.ParseUint(gb, 10, 64)
+		zBytes *= GB
+	} else if strings.HasSuffix(str, "MB") {
+		mb := strings.Trim(str, "MB")
+		zBytes, err = strconv.ParseUint(mb, 10, 64)
+		zBytes *= MB
+	} else if strings.HasSuffix(str, "KB") {
+		kb := strings.Trim(str, "KB")
+		zBytes, err = strconv.ParseUint(kb, 10, 64)
+		zBytes *= KB
+	} else if strings.HasSuffix(str, "B") {
+		return 0, fmt.Errorf("Unparsable bytes string: %s", str)
+	}
+
+	return zBytes, err
 }
